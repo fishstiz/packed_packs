bd19accf0cb30e50ea61eb5a2b63f4bcbf152fdc
fishstiz <fishstickz7777@gmail.com>
2026-03-13 14:34:51 +0800

    fix closing folder scrolling to last selected

--- mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/PackListContainer.java
+++ mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/PackListContainer.java
@@ -79,10 +79,6 @@ public class PackListContainer extends AbstractWidget implements FocusPathProvid
     private void updateState(GuiEventListener target) {
         this.children = List.of(target);
         this.setFocused(target);
-        ComponentPath path = target.nextFocusPath(new FocusNavigationEvent.InitialFocus());
-        if (path != null) {
-            path.applyFocus(true);
-        }
     }
 
     private void refresh() {
@@ -95,6 +91,7 @@ public class PackListContainer extends AbstractWidget implements FocusPathProvid
             Folder newFolder = new Folder(this.root == null ? this : this.root, this.viewModel.createFolderSlice());
             this.folder = newFolder;
             this.updateState(newFolder);
+            ObjectsUtil.ifPresent(newFolder.nextFocusPath(new FocusNavigationEvent.InitialFocus()), path -> path.applyFocus(true));
         } else {
             this.folder = null;
             this.updateState(this.packList);
