a05025b0708630662c008806264e2bfde674eac9
fishstiz <fishstickz7777@gmail.com>
2026-04-11 04:30:20 +0800

    fix minor leak when toggling dev mode

--- mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/screens/PackedPacksScreen.java
+++ mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/screens/PackedPacksScreen.java
@@ -181,7 +181,7 @@ public class PackedPacksScreen extends Screen implements HoverStateHandler, Togg
                 .setOnPress(this::toggleActionBar)
                 .build()));
 
-        header.addFlexChild(this.profilesSidebar.createProfileHeader());
+        header.addFlexChild(this.profilesSidebar.getProfileHeader());
 
         this.addExtensions(header, InitializeLayoutEvent.Pos.AFTER_TITLE, extensions);
 
