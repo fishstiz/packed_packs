ab5f079c832e7a34215805ae37063dd72b754e5e
fishstiz <fishstickz7777@gmail.com>
2026-06-03 01:48:10 +0800

    fix last selected resolution and inconsistent move order

--- mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/components/PackList.java
+++ mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/components/PackList.java
@@ -251,7 +251,7 @@ public class PackList extends FZAbstractListWidget<PackList.Entry> implements Fo
 
     private @Nullable Entry getSelected() {
         for (Entry entry : children()) {
-            if (entry.entryModel.selected()) {
+            if (entry.entryModel.selectedLast()) {
                 return entry;
             }
         }
