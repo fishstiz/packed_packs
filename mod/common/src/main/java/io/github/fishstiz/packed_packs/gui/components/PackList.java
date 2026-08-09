273963524e8630ae1ad68469902dbac03980447d
fishstiz <fishstickz7777@gmail.com>
2026-08-10 06:10:40 +0800

    unconstrain arrow key navigation in pack list

--- mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/components/PackList.java
+++ mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/components/PackList.java
@@ -277,10 +277,10 @@ public class PackList extends FZAbstractListWidget<PackList.Entry> implements Fo
         Entry selectedEntry = getFocusedOrSelected();
         return switch (direction) {
             case UP -> Objects.equals(selectedEntry, children().getFirst())
-                    ? selectedEntry
+                    ? null
                     : getPreviousEntry(selectedEntry);
             case DOWN -> Objects.equals(selectedEntry, children().getLast())
-                    ? selectedEntry
+                    ? null
                     : getNextEntry(selectedEntry);
             case LEFT -> key().type().available() ? selectedEntry : null;
             case RIGHT -> key().type().enabled() ? selectedEntry : null;
@@ -360,8 +360,9 @@ public class PackList extends FZAbstractListWidget<PackList.Entry> implements Fo
             return true;
         }
         if (isUp(keyEvent) || isDown(keyEvent)) {
-            selectOnKeyPress(getNextEntryAt(isUp(keyEvent) ? ScreenDirection.UP : ScreenDirection.DOWN));
-            return true;
+            Entry nextEntry = getNextEntryAt(isUp(keyEvent) ? ScreenDirection.UP : ScreenDirection.DOWN);
+            selectOnKeyPress(nextEntry);
+            return nextEntry != null;
         }
         if (isHome(keyEvent) || isEnd(keyEvent)) {
             if (children().isEmpty()) return true;
