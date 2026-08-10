c5f94d54d6d41e7e7723d49b17615ea3746e7457
fishstiz <fishstickz7777@gmail.com>
2026-08-10 11:12:02 +0800

    audit mixins in testmod

--- testmod/common/src/main/java/io/github/fishstiz/testmod/mixin/MinecraftMixin.java
+++ testmod/common/src/main/java/io/github/fishstiz/testmod/mixin/MinecraftMixin.java
@@ -4,14 +4,23 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.github.fishstiz.testmod.pack.TestPackRegistry;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.main.GameConfig;
 import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.server.packs.repository.RepositorySource;
 import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void auditMixins(GameConfig gameConfig, CallbackInfo ci) {
+        MixinEnvironment.getCurrentEnvironment().audit();
+    }
+
     @WrapOperation(method = "<init>", at = @At(
             value = "NEW",
             target = "([Lnet/minecraft/server/packs/repository/RepositorySource;)Lnet/minecraft/server/packs/repository/PackRepository;")
