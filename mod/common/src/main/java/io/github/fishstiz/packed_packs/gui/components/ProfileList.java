2dab9d76b87f85700fd010d0df253b34fc777750
fishstiz <fishstickz7777@gmail.com>
2026-08-10 15:20:31 +0800

    only show load condition options for default profile

--- mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/components/ProfileList.java
+++ mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/components/ProfileList.java
@@ -216,7 +216,7 @@ public class ProfileList extends FZAbstractListWidget<ProfileList.Entry> impleme
                     .icon(createIcon(() -> model.isLocked() ? LOCK_SPRITE_SMALL : UNLOCK_SPRITE_SMALL))
                     .onPress(model::toggleLock)));
 
-            if (context.isServerData()) {
+            if (!model.isDefault() || context.isServerData()) {
                 return;
             }
 
