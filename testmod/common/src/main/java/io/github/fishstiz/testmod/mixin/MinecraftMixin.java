e56e2413124ef84db0c035a5159edee1c8bec487
fishstiz <fishstickz7777@gmail.com>
2026-08-10 11:12:02 +0800

    audit mixins in testmod

--- testmod/common/src/main/java/io/github/fishstiz/testmod/mixin/MinecraftMixin.java
+++ testmod/common/src/main/java/io/github/fishstiz/testmod/mixin/MinecraftMixin.java
@@ -8,10 +8,15 @@ import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.server.packs.repository.RepositorySource;
 import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
+    static {
+        MixinEnvironment.getCurrentEnvironment().audit();
+    }
+
     @WrapOperation(method = "<init>", at = @At(
             value = "NEW",
             target = "([Lnet/minecraft/server/packs/repository/RepositorySource;)Lnet/minecraft/server/packs/repository/PackRepository;")
