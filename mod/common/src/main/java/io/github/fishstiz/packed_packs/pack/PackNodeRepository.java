26de3d42949660ac79b17cb834e900adf54190c0
fishstiz <fishstickz7777@gmail.com>
2026-09-22 23:28:56 +0800

    fix nested packs being in reversed order when enabled

--- mod/common/src/main/java/io/github/fishstiz/packed_packs/pack/PackNodeRepository.java
+++ mod/common/src/main/java/io/github/fishstiz/packed_packs/pack/PackNodeRepository.java
@@ -348,14 +348,14 @@ public class PackNodeRepository {
             packIds.add(pack.getId());
         };
 
-        for (PackNode pack : packs.reversed()) {
+        for (PackNode pack : packs) {
             switch (pack) {
                 case PackNode.Leaf leaf -> collector.accept(leaf.pack());
                 case PackNode.Parent parent -> collectPacks(parent, collector);
             }
         }
 
-        this.repository.setSelected(ImmutableList.copyOf(packIds));
+        this.repository.setSelected(ImmutableList.copyOf(packIds.reversed()));
 
         OptionInstance<@NonNull Boolean> highContrastOption = Minecraft.getInstance().options.highContrast();
         if (highContrastOption.get() != highContrast.booleanValue()) {
