07d1fa9a3acd0f1170b84203d829249e06067c4c
fishstiz <fishstickz7777@gmail.com>
2026-09-23 19:50:27 +0800

    readd focus searchfield shorcut key

--- mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/screens/PackedPacksScreen.java
+++ mod/common/src/main/java/io/github/fishstiz/packed_packs/gui/screens/PackedPacksScreen.java
@@ -702,6 +702,14 @@ public class PackedPacksScreen extends FZScreen {
                 return searchField.keyPressed(event);
             }
         }
+        if (isSearch(event)) {
+            FZTextField searchField = getClosestSearchField();
+            if (searchField != null && !searchField.isFocused()) {
+                ribbonOpen.set(true);
+                setFocused(searchField);
+                return true;
+            }
+        }
         return false;
     }
 
